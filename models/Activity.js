e936eabe5d3604e647bb7677737be275fc785af1
MOMISBACK <46964435+MOMISBACK@users.noreply.github.com>
2025-12-12 17:01:56 +0100

    Add files via upload

--- models/Activity.js
+++ models/Activity.js
@@ -1,25 +1,39 @@
 const mongoose = require('mongoose');
 
 const activitySchema = new mongoose.Schema({
-  userId: {
+  user: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
     required: true,
+    ref: 'User',
+  },
+  title: {
+    type: String,
+    required: [true, 'Please add a title'],
+    trim: true,
   },
   type: {
     type: String,
-  },
-  distance: {
-    type: Number,
+    required: [true, 'Please add an activity type'],
+    enum: ['course', 'velo', 'natation', 'marche'],
   },
   duration: {
     type: Number,
+    required: [true, 'Please add a duration in minutes'],
+  },
+  distance: {
+    type: Number,
+    required: false,
+  },
+  calories: {
+    type: Number,
+    required: false,
   },
   date: {
     type: Date,
+    default: Date.now,
   },
+}, {
+  timestamps: true,
 });
 
-const Activity = mongoose.model('Activity', activitySchema);
-
-module.exports = Activity;
+module.exports = mongoose.model('Activity', activitySchema);
