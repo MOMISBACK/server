60b211c2528999bbc671270a6da4abc8a7d0f7cc
MOMISBACK <46964435+MOMISBACK@users.noreply.github.com>
2025-12-16 06:45:22 +0100

    Delete routes directory

--- routes/testRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { sendTestEmail } = require('../controllers/testController');
-
-router.post('/send-test-email', sendTestEmail);
-
-module.exports = router;
